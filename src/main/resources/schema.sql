cbc1f0dbce51b70e6c1ceefa47d1af62d74d2946
keyy1315 <lovelina1315@gmail.com>
2025-03-07 10:37:13 +0900

    chore: fix schema.sql
    sql문 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS `member`
 (
     `id` bigint(20) NOT NULL,
-    `email` varchar(255) NOT NULL UNIQUE ,
-    `password` varchar(255) DEFAULT NULL,
+    `user_id` varchar(255) NOT NULL UNIQUE,
+    `user_name` varchar(255) DEFAULT NULL,
+    `name_tag` varchar(255) DEFAULT NULL
+    `password` varchar(255) NOT NULL,
     `role` enum('ADMIN','USER') NOT NULL DEFAULT 'USER',
     `tier` varchar(255) DEFAULT NULL,
     `isActive` tinyint(1) NOT NULL DEFAULT 1,
+    `refresh_token` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
